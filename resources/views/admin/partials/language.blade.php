b6e1d57be52bb3f15c13386b740fa453f7e36e56
Khaled Abdurahman <khaledabdurahman5@gmail.com>
2026-05-06 22:37:22 +0300

    Added localization and switching using mcamara packages

---
+++ resources/views/admin/partials/language.blade.php
@@ -0,0 +1,3 @@
+<a class="nav-link text-muted my-2" href="{{LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale() == 'ar' ? 'en' : 'ar')}}" id="langSwitcher">
+  {{LaravelLocalization::getCurrentLocale() == 'ar' ? 'EN' : 'العربية'}}
+</a>
