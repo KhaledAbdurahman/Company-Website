4ec9ba6c9ccc63da346b7454c1e4f93ead4877ee
Khaled Abdurahman <khaledabdurahman5@gmail.com>
2026-05-12 10:23:46 +0300

    Features & Messages Modules -For Admin- Implemented

--- resources/views/admin/features/show.blade.php
+++ resources/views/admin/features/show.blade.php
@@ -1,12 +1,12 @@
 @extends('admin.master')
 
-@section('title', __('keywords.show_service'))
+@section('title', __('keywords.show_feature'))
 
 @section('content')
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-12">
-                <h2 class="h5 page-title">{{ __('keywords.show_service') }}</h2>
+                <h2 class="h5 page-title">{{ __('keywords.show_feature') }}</h2>
 
                 <div class="card shadow">
                     <div class="card-body">
@@ -15,21 +15,21 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="title">{{ __('keywords.title') }}</label>
-                                    <p class="form-control">{{ $service->title }}</p>
+                                    <p class="form-control">{{ $feature->title }}</p>
                                 </div>
                             </div>
 
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="icon">{{ __('keywords.icon') }}</label>
-                                    <p class="form-control">{{ $service->icon }}</p>
+                                    <p class="form-control">{{ $feature->icon }}</p>
                                 </div>
                             </div>
 
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <label for="description">{{ __('keywords.description') }}</label>
-                                    <p class="form-control">{{ $service->description }}</p>
+                                    <p class="form-control">{{ $feature->description }}</p>
                                 </div>
                             </div>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\FeatureController;
+use App\Http\Controllers\MessageController;
 
 /*
 * Front Routes
@@ -23,8 +25,19 @@ Route::name('admin.')->prefix(LaravelLocalization::setLocale() . '/admin')->midd
     Route::middleware('auth')->group(function(){
         Route::view('/', 'admin.index')->name('index');
 
+        // ======================================= Services
         Route::controller(ServiceController::class)->group(function(){
             Route::resource('services', ServiceController::class);
+        });
+
+        // ======================================= Features
+        Route::controller(FeatureController::class)->group(function(){
+            Route::resource('features', FeatureController::class);
+        });
+
+        // ======================================= Messages
+        Route::controller(MessageController::class)->group(function(){
+            Route::resource('messages', MessageController::class)->only(['index', 'show', 'destroy']);
         });
     });
 
