4a51a7aac78b70f48f204582fddc777d4fdcec1f
Khaled Abdurahman <khaledabdurahman5@gmail.com>
2026-05-18 09:08:28 +0300

    Features Added to new Component

---
+++ app/View/Components/FrontFeaturesComponent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use App\Models\Feature;
+
+class FrontFeaturesComponent extends Component
+{
+    public $features;
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        $this->features = Feature::all();
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.front-features-component');
+    }
+}
